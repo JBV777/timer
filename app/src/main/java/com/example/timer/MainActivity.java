e10781d878d63aa5c438aef37ff9c66244afdf8e
owner <shenxu183@yahoo.com>
2018-11-19 21:53:23 -0400

    Made the button "start timer" in prompt.xml functional.
    The button allows user to go from prompt.xml to countdown.xml

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.timer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.prompt);
+        //links button with "Start timer" button on prompt
+        Button start=(Button) findViewById(R.id.button2);
+        start.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                setContentView(R.layout.countdown);
+            }
+
+
+        });
+
     }
 }
